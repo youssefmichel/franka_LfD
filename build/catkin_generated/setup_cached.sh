2c4f8f6b6a42e02b73fdce2811a24d0a634be314
youssef <youssef.abdelwadoud@tum.de>
2024-04-19 17:57:34 +0200

    Initial commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export ROSLISP_PACKAGE_DIRECTORIES='/home/dhrikarl/Codes/franka_ws/src/franka_LfD/build/devel/share/common-lisp'
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/dhrikarl/Codes/franka_ws/src/franka_LfD/build/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/dhrikarl/Codes/franka_ws/src/franka_LfD/build'
+export ROS_PACKAGE_PATH="/home/dhrikarl/Codes/franka_ws/src/franka_LfD:$ROS_PACKAGE_PATH"
